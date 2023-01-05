797420f1cc631f58b181cd1f1a7ca175f2ab45ce
Moin MN <100254727+MoinMN@users.noreply.github.com>
2023-01-05 12:41:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
+let url = new URL(window.location.href);
+url.search = '';
+
+const result = url.toString();
+
+if(window.location.href != result || window.location.href != "http://127.0.0.1:5500/MumbaiLocal/index.html"){
+    window.location.href = result
+}
+
 let btn = document.getElementById('btn');
-let content = document.getElementById('hide-content');
+let hide_content = document.getElementById('hide-content');
 function show_hide(){
-    if(content.style.display == 'none'){
-        content.style.display = 'block';
+    if(hide_content.style.display == 'none'){
+        hide_content.style.display = 'block';
         btn.innerHTML = 'Hide';
     }
     else{
-        content.style.display = 'none';
+        hide_content.style.display = 'none';
         btn.innerHTML = 'Show';
     }
+}
+
+
+const searchFun=()=>{
+    let search = document.getElementById('search').value.toUpperCase();
+    // console.log(search);
+
+    let content = document.getElementById('content');
+    let station = content.getElementsByTagName('h3');
+
+    for(var i=0; i<station.length; i++) {
+        let ss = station[i].innerHTML.toUpperCase();
+
+        // console.log(ss.slice(11));
+        // console.log(station[1].getAttribute('id'));
+
+        let a = ss.indexOf(search);
+        let attri = station[i].getAttribute('id');
+        
+        if (a > -1) {
+            window.location.href = '#'+attri;
+        }     
+    }
+}
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
