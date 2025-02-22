568eab057a5a89659870327b9bc38d90832288bc
MoinMN <moinnaik45@eng.rizvi.edu.in>
2025-02-22 11:50:41 +0530

    improve seo

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
